f4c130d6474732eba56040c8284688be7059400f
Diogo Pinto <diogo.alvespinto@gmail.com>
2023-08-14 12:18:14 +0100

    working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,14 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @answer = ''
+
+    if params[:question][-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif params[:question] != 'I am going to work right now!'
+      @answer = "I don't care, get dressed and go to work!"
+    else
+      @answer = 'Great!'
+    end
   end
 end
